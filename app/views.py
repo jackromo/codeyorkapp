82cc1da64a2fb8b472af8a3c80f31c35975a1f79
Jack <sharrackor@gmail.com>
2016-09-08 05:35:31 +0200

    Add empty homepage

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def hello_world():
-    return 'Hello World!'
+    return render_template("partials/home.html")
