333893a623215898c77d0e65cae6e9ff85fc6094
Jack <sharrackor@gmail.com>
2016-09-09 23:07:12 +0200

    Add file logging in non-debug mode

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 
 app = Flask(__name__)
@@ -15,8 +15,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-# mail server init
 if not app.debug:
+    # mail server init
     credentials = (MAIL_USERNAME, MAIL_PASSWORD) if MAIL_USERNAME or MAIL_PASSWORD else None
     mail_handler = SMTPHandler(
         (MAIL_SERVER, MAIL_PORT),
@@ -27,6 +27,16 @@ if not app.debug:
     )
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+    # file logging init
+    # 1 megabyte log files, keep last 10 log files as backup
+    file_handler = RotatingFileHandler('tmp/codeyorkapp.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(
+        logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('CodeYork app startup')
 
 
 from app import views, models
