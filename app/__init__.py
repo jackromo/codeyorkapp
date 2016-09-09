f7c5aadd3464d176ccebde798709d7829478e786
Jack <sharrackor@gmail.com>
2016-09-09 22:52:38 +0200

    Enable error log emails to admins

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+import logging
+from logging.handlers import SMTPHandler
 
 
 app = Flask(__name__)
@@ -11,5 +14,19 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+# mail server init
+if not app.debug:
+    credentials = (MAIL_USERNAME, MAIL_PASSWORD) if MAIL_USERNAME or MAIL_PASSWORD else None
+    mail_handler = SMTPHandler(
+        (MAIL_SERVER, MAIL_PORT),
+        'no-reply@' + MAIL_SERVER,
+        ADMINS,
+        'CodeYork app failure',
+        credentials
+    )
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
 
 from app import views, models
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 # WTForm Config
 
 WTF_CSRF_ENABLED = True
@@ -8,8 +10,15 @@ SECRET_KEY = 'i-totally-thought-of-a-good-key-m8-lmao-get-rekked-hax0rs'
 
 # SQLite Config
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True  # set to True for debugging
+
+# Mail Server Config
+
+MAIL_SERVER = 'parseltongue.net'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+ADMINS = ['sharrackor@gmail.com']
